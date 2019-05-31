300052f0a57e450834c16870424c66ac74b10f3f
xyuan <coraxyuan@gmail.com>
2019-05-31 09:58:44 -0400

    Add semi-clean GSA SCI data

--- scripts/hhi_processing.R
+++ scripts/hhi_processing.R
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #=================================================================================================================#
 
 # Data processing for ContractingOffice-based HHI Data
@@ -67,3 +68,51 @@ fy_summary_nc<-contracting_office_naics_nc %>%
     numberOfContracts=sum(numberOfContracts),
     hh_index=sum((pct*100)^2,na.rm=TRUE)
   )
+=======
+#=================================================================================================================#
+
+# Data processing for ContractingOffice-based HHI Data
+
+#=================================================================================================================#
+
+library(tidyverse)
+library(csis360)
+library(dplyr)
+library(readr)
+
+contracting_office_naics = read.table("data//semi_clean//Office.OfficeIDhistoryNAICS_fy.txt", header = TRUE, 
+                                      na.strings = c("","NA","NULL"),
+                                      quote = "\"",
+                                      sep = "\t")
+
+names(contracting_office_naics) <- c("Fiscal_year","principalNAICScode",
+                                     "principalnaicscodeText","ContractingOfficeCode",
+                                     "numberOfContracts",
+                                     "obligatedAmount","numberOfActions")
+
+contracting_office_naics<-contracting_office_naics %>% 
+  group_by(Fiscal_year,ContractingOfficeCode) %>%
+  filter(Fiscal_year > 2000) 
+  
+
+#Rank and calculated HHI by obligation amount
+contracting_office_naics_ob<-contracting_office_naics %>%
+  dplyr::mutate(
+    pos = rank(-obligatedAmount,
+               ties.method ="min"),
+    pctObl = ifelse(obligatedAmount>0,
+                 obligatedAmount / sum(obligatedAmount[obligatedAmount>0]),
+                 NA),
+    pctK = numberOfContracts / sum(numberOfContracts)
+  )
+
+office_naics_hhi<-contracting_office_naics_ob %>%
+  dplyr::summarize(
+    obligatedAmount = sum(obligatedAmount),
+    numberOfContracts=sum(numberOfContracts),
+    hh_index_obl=sum((pctObl*100)^2,na.rm=TRUE),
+    hh_index_k=sum((pctK*100)^2,na.rm=TRUE)
+  )
+
+write.csv(office_naics_hhi,"data//clean//office_naics_hhi.csv")
+>>>>>>> Stashed changes
